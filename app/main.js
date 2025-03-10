74874f0033f95983662672b56546ff74e385cc84
Shadowdara <gaw@ve>
2025-03-10 16:05:09 +0100

    [any message]

--- app/main.js
+++ app/main.js
@@ -45,7 +45,6 @@ function printtype(cmdName) {
       const fullPath = path.join(p, cmdName);
       if(fs.existsSync(fullPath) && fs.statSync(fullPath).isFile()) {
         console.log(`${cmdName} is ${fullPath}`);
-        console.log("hello")
         found = true;
       }
     }
