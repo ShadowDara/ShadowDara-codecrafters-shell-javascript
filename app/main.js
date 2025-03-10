249ec58fd97de60fce45a31ccff48e580f0f02bc
Shadowdara <gaw@ve>
2025-03-10 12:04:09 +0100

    [any message]

--- app/main.js
+++ app/main.js
@@ -7,8 +7,7 @@ const rl = readline.createInterface({
 
 function checkanswer(answer) {
   if (answer == "exit 0" || answer == "0"){
-    rl.close();
-    console.log(`exit 0`);
+    process.exit(0);
   //} else if (answer == "1") {
   } else {
     console.log(`${answer}: command not found`);
